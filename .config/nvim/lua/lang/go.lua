8a2494cdebf6766f897d09c6e2de7d57b054cc01
hank <hankmo.x@gmail.com>
2025-03-21 14:09:22 +0000

    feat: use neo-tree

--- .config/nvim/lua/lang/go.lua
+++ .config/nvim/lua/lang/go.lua
@@ -10,6 +10,20 @@ vim.api.nvim_create_autocmd("FileType", {
 
 return {
   {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
     "olexsmir/gopher.nvim",
     event = "VeryLazy",
     ft = "go",
