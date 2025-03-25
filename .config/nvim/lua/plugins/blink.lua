973bc3b02ca2a194a9306f434802be365f6c410c
hank <hankmo.x@gmail.com>
2025-03-25 06:26:29 +0000

    feat: new plugins and config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -16,33 +16,5 @@ return {
         ["<C-n>"] = { "select_next", "fallback" },
       },
     },
-    -- opts = {
-    --   -- 'default' for mappings similar to built-in completion
-    --   -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    --   -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    --   -- See the full "keymap" documentation for information on defining your own keymap.
-    --   keymap = {
-    --     -- set to 'none' to disable the 'default' preset
-    --     -- preset = "enter",
-    --
-    --     ["<M-Tab>"] = { "select_prev", "fallback" },
-    --     ["<Tab>"] = { "select_next", "fallback" },
-    --
-    --     -- -- disable a keymap from the preset
-    --     -- -- ["<C-e>"] = {},
-    --     --
-    --     -- -- show with a list of providers
-    --     -- ["<C-space>"] = {
-    --     --   function(cmp)
-    --     --     cmp.show({ providers = { "snippets" } })
-    --     --   end,
-    --     -- },
-    --   },
-    --   snippets = { preset = "luasnip" },
-    --   -- ensure you have the `snippets` source (enabled by default)
-    --   sources = {
-    --     default = { "lsp", "path", "snippets", "buffer" },
-    --   },
-    -- },
   },
 }
