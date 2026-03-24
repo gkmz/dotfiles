0474a67e28a000e415b6149b6765225de15da3b7
hankmo <hankmo.x@gmail.com>
2026-03-24 11:14:57 +0800

    feat: 关闭自动格式化

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,3 +8,4 @@ vim.opt.mouse = "a" -- Enable mouse support
 vim.opt.guifont = "JetBrainsMono Nerd Font:h14" -- Prefer high-quality coding font
 vim.opt.wrap = true -- Always enable soft wrapping
 vim.opt.linebreak = true -- Wrap at word boundaries
+vim.g.autoformat = false -- Turn off LazyVim's autoformat-on-save
